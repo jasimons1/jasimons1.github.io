19deadd0c1a2f33396672da595a90ebf56404f38
jasimons1 <ja.simons1@ad-academie.nl>
2023-02-27 18:06:18 +0100

    Aanpassen CSS

--- src/pages/Lotr.js
+++ src/pages/Lotr.js
@@ -17,27 +17,30 @@ export default function Lotr() {
         <div>
             <div className="lotr">
                 <h1 className="title">The Lord of the Rings</h1>
-                <div className="slider"><ImageSlider images={sliderImages}/></div>
-                <div className="text">
-                    <p>
-                        <h2>Chapter I</h2>
-                        <h3>A Long-expected Party</h3>
-                        <p>When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his
-                            eleventy-first birthday with a party of special magnificence, there was much talk and
-                            excitement in Hobbiton.</p>
-                        <p>Bilbo was very rich and very peculiar, and had been the wonder of the Shire for sixty years,
-                            ever since his remarkable disappearance and unexpected return. The riches he had brought
-                            back from his travels had now become a local legend, and it was popularly believed, whatever
-                            the old folk might say, that the Hill at Bag End was full of tunnels stuffed with
-                            treasure.</p>
-                        <p>And if that was not enough for fame, there was also his prolonged vigour to marvel at. Time
-                            wore on, but it seemed to have little effect on Mr. Baggins. At ninety he was much the same
-                            as at fifty. At ninety-nine they began to call him well-preserved; but unchanged would have
-                            been nearer the mark. There were some that shook their heads and thought this was too much
-                            of a good thing; it seemed unfair that anyone should possess (apparently) perpetual youth as
-                            well as (reputedly) inexhaustible wealth.</p>
-                        <p>‘It will have to be paid for,’ they said. ‘It isn’t natural, and trouble will come of it!</p>
-                    </p>
+                <div className="container">
+                    <div className="text">
+                        <p>
+                            <h2>Chapter I</h2>
+                            <h3>A Long-expected Party</h3>
+                            <p>When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his
+                                eleventy-first birthday with a party of special magnificence, there was much talk and
+                                excitement in Hobbiton.</p>
+                            <p>Bilbo was very rich and very peculiar, and had been the wonder of the Shire for sixty
+                                years, ever since his remarkable disappearance and unexpected return. The riches he had
+                                brought back from his travels had now become a local legend, and it was popularly
+                                believed, whatever the old folk might say, that the Hill at Bag End was full of tunnels
+                                stuffed with treasure.</p>
+                            <p>And if that was not enough for fame, there was also his prolonged vigour to marvel at.
+                                Time wore on, but it seemed to have little effect on Mr. Baggins. At ninety he was much
+                                the same as at fifty. At ninety-nine they began to call him well-preserved; but
+                                unchanged would have been nearer the mark. There were some that shook their heads and
+                                thought this was too much of a good thing; it seemed unfair that anyone should possess
+                                (apparently) perpetual youth as well as (reputedly) inexhaustible wealth.</p>
+                            <p>‘It will have to be paid for,’ they said. ‘It isn’t natural, and trouble will come of
+                                it!</p>
+                        </p>
+                    </div>
+                    <div className="slider"><ImageSlider images={sliderImages}/></div>
                 </div>
             </div>
         </div>
