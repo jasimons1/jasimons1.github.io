f552ac5500b7fd1acfdb0fd141abc523c45f19c3
jasimons1 <ja.simons1@ad-academie.nl>
2023-12-19 17:17:25 +0100

    - Styles adjusted - Added possibility to ask random questions from JSON file - Added a filter so character guessing questions don't get asked first

--- src/components/UsernameInput.js
+++ src/components/UsernameInput.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const UsernameInput = ({ setUsername }) => {
-    const [inputValue, setInputValue] = useState('');
+const UsernameInput = ({ setUsername, setRandomQuestion }) => {
+    const [inputValue, setInputValue] = useState('')
 
     const handleInputChange = (event) => {
-        setInputValue(event.target.value);
+        setInputValue(event.target.value)
     };
 
     const handleSubmit = (event) => {
-        event.preventDefault();
-        setUsername(inputValue);
-    };
+        event.preventDefault()
+        setUsername(inputValue)
+        setRandomQuestion()
+    }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <label className="label">
-                    Enter your username:
-                </label>
-                <input type="text" value={inputValue} onChange={handleInputChange} />
-                <button type="submit">Play</button>
+                <label className="label">Enter your username:</label>
+                <input className="inputField" type="text" value={inputValue} onChange={handleInputChange} />
+                <button className="playButton" type="submit">Play</button>
             </form>
         </div>
-    );
-};
+    )
+}
 
-export default UsernameInput;
+export default UsernameInput
--- src/pages/TheAllSeeingEye.js
+++ src/pages/TheAllSeeingEye.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
-import UsernameInput from "../components/UsernameInput";
+import UsernameInput from "../components/UsernameInput"
+import questionsData from "../json/questions.json"
 
 import eyeOfSauron from "../images/eyeOfSauron.png"
 
 export default function TheAllSeeingEye() {
     const [username, setUsername] = useState('')
+    const [randomQuestion, setRandomQuestion] = useState(null)
+
+    const getRandomQuestion = () => {
+        const excludedIDs = Array.from({length: 28 }, (_, index) => index + 27);
+        const filteredQuestions = questionsData.questions.filter(
+            (question) => !excludedIDs.includes(question.id)
+        )
+
+        const randomIndex = Math.floor(Math.random() * filteredQuestions.length)
+        const question = filteredQuestions[randomIndex]
+        setRandomQuestion(question)
+    }
 
     return (
         <div className="home">
@@ -16,17 +29,25 @@ export default function TheAllSeeingEye() {
                 <div className="quizContainer">
                     <div className="userInputField">
                         {username ? (
-                            <h2>Welcome, {username}!</h2>
+                            <>
+                            <div className="questions">
+                                <h2>Welcome {username}!</h2>
+                                {randomQuestion && (
+                                    <>
+                                        <input type="text" value={randomQuestion.text} readOnly />
+                                    </>
+                                )}
+                            </div>
+                            </>
                         ) : (
                             <>
                                 <div className="welcomeText">
                                     <h2>Welcome to The All Seeing Eye! Would you like to play a game?</h2>
                                 </div>
-                                <UsernameInput setUsername={setUsername} />
+                                <UsernameInput setUsername={setUsername} setRandomQuestion={getRandomQuestion} />
                             </>
                         )}
                     </div>
-                    {/* Other game content goes here */}
                 </div>
             </div>
         </div>
